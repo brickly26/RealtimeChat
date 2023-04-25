bb429f00f7cfd62ff2b1cf3c24ca5f068df2b36e
brickly26 <sabburak26@gmail.com>
2023-04-25 11:38:52 -0400

    first commit

---
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Button from "@/components/ui/Button";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import { FC } from "react";
+
+const page = async ({}) => {
+  return <div>hello</div>;
+};
+
+export default page;
